2b3d34cd648592f5da044127140e694c7171cb81
Neylton Benjamim <neylton.b@hotmail.com>
2022-02-22 20:54:23 -0300

    Exemplo de uso do tranformação direto no construtor

--- src/Exemplos/BaseString.php
+++ src/Exemplos/BaseString.php
@@ -1,13 +1,26 @@
 <?php
 require "../../vendor/autoload.php";
 
+use Type\BaseString;
 use Type\Nome;
 
-$base = new Nome('neylton Benjamim dos anjos');
+$nome = new Nome('neylton Benjamim dos anjos');
 
 
-echo "<p><b>Original:</b> {$base->getString()}</p>";
-echo "<p><b>Upper:</b> {$base->upper()}</p>";
-echo "<p><b>Lower:</b> {$base->lower()}</p>";
-echo "<p><b>UC First:</b> {$base->ucfirst()}</p>";
-echo "<p><b>UC Words:</b> {$base->ucwords()}</p>";
+echo "<p><b>Original:</b> {$nome->getString()}</p>";
+echo "<p><b>Upper:</b> {$nome->upper()}</p>";
+echo "<p><b>Lower:</b> {$nome->lower()}</p>";
+echo "<p><b>UC First:</b> {$nome->ucfirst()}</p>";
+echo "<p><b>UC Words:</b> {$nome->ucwords()}</p>";
+echo '<hr>';
+echo 'COM CONST: ....<br>';
+$nome = new Nome('neylton benjamim');
+echo "<p><b>Original:</b> {$nome}</p>";
+$nome = new Nome('neylton benjamim', BaseString::UPPER);
+echo "<p><b>Upper:</b> {$nome}</p>";
+$nome = new Nome('neylton benjamim', BaseString::LOWER);
+echo "<p><b>Lower:</b> {$nome}</p>";
+$nome = new Nome('neylton benjamim', BaseString::UC_FIRST);
+echo "<p><b>UC First:</b> {$nome}</p>";
+$nome = new Nome('neylton benjamim', BaseString::UC_WORDS);
+echo "<p><b>UC Words:</b> {$nome}</p>";
